b163fc91f454abc45eb017f912d75a69f6db69b0
Oleksandr Knyga <oleksandrknyga@gmail.com>
2014-02-17 02:28:24 +0200

    mit

--- WebExtractor/Client/Decorator/ClientResponseCacheDecorator.php
+++ WebExtractor/Client/Decorator/ClientResponseCacheDecorator.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * This file is part of the WebExtractor package.
+ *
+ * 
+ * (c) Sobit Akhmedov <sobit.akhmedov@gmail.com>, Oleksandr Knyga <oleksandrknyga@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace WebExtractor\Client\Decorator;
 
 use WebExtractor\Client\ClientInterface;
